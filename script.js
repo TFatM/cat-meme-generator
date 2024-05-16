ca6a0db62fbd8f4d5db5611bca97506edbfe0613
Vitaliy Butsai <150235951+TFatM@users.noreply.github.com>
2024-05-16 10:58:12 +0300

    Add files via upload
    Added more buttons. Now you can download your meme in png format. Added cat more image

--- script.js
+++ script.js
@@ -1,20 +1,71 @@
+let memes = [
+  {
+    img: "./images/Cute-Cat.jpg"
+  },
+  {
+    img: "./images/black-cat-zoning-out.png"
+  },
+  {
+    img: "./images/crying-cat.jpg"
+  },
+  {
+    img: "./images/loading-cat.jpg"
+  },
+  {
+    img: "./images/sad-cat.jpg"
+  },
+  {
+    img: "./images/Take-A-Seat-Cat.jpg"
+  },
+  {
+    img: "./images/worshipping-a-cat.png"
+  }
+];
+let memeIndex = 0;
 let image = document.querySelector(".image");
+let container = document.querySelector(".container");
 let topText = document.querySelector(".top-text");
 let bottomText = document.querySelector(".bottom-text");
 let topInput = document.querySelector(".top-input");
 let bottomInput = document.querySelector(".bottom-input");
-let generateBtn = document.querySelector(".generate-btn");
+let changeBtn = document.querySelector(".change-btn");
+let nextBtn = document.querySelector(".next-btn");
+let prevBtn = document.querySelector(".prev-btn");
 let downloadBtn = document.querySelector(".download-btn");
 
-function generateMeme() {
+function changeText() {
   topText.textContent = topInput.value;
   bottomText.textContent = bottomInput.value;
+};
+
+function nextImage() {
+  if (memeIndex === memes.length - 1) {
+    memeIndex = 0;
+  } else {
+    memeIndex += 1;
+  }
+
+  image.src = memes[memeIndex].img;
+};
+
+function previousImage() {
+    if (memeIndex === memes.length - 1) {
+    memeIndex = 0;
+  } else {
+      memeIndex -= 1;
+  }
+
+  image.src = memes[memeIndex].img;
 }
 
 function downloadMeme() {
-        html2canvas(document.querySelector(".container")).then(canvas => {
-          document.body.appendChild(canvas);
-          downloadBtn.href = canvas.toDataURL("image/png");
-          // downloadBtn.style.display = 'inline';
-        });
-      }
+  const screenshotTarget = document.querySelector(".container");
+  html2canvas(screenshotTarget).then((canvas) => {
+    const base64image = canvas.toDataURL("image/png");
+    var anchor = document.createElement('a');
+    anchor.setAttribute("href", base64image);
+    anchor.setAttribute("download", "my-cat-meme.png");
+    anchor.click();
+    anchor.remove();
+  });
+};
